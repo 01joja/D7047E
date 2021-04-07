5c464829eee15e7f6d2a7991a3cb334f4bcf7391
Codemaster1337 <hampib53@gmail.com>
2021-04-07 17:56:19 +0200

    Advanced random guesser

--- Practical1.py
+++ Practical1.py
@@ -6,13 +6,16 @@ from torch.utils.data import DataLoader
 import copy
 
 batch_size = 10000
-epochs = 15
+epochs = 1
 
-cifar_train = datasets.CIFAR10("./", train=True, download=True, transform=transforms.ToTensor())
-train_loader = DataLoader(cifar_train, batch_size=batch_size, shuffle=False)
-
+cifar_data = datasets.CIFAR10("./", train=True, download=True, transform=transforms.ToTensor())
 cifar_test = datasets.CIFAR10("./", train=False, download=True, transform=transforms.ToTensor())
-test_loader = DataLoader(cifar_test, batch_size=batch_size, shuffle=False)
+
+cifar_train, cifar_val = torch.utils.data.random_split(cifar_data,[40000,10000],generator=torch.Generator().manual_seed(420))
+
+train_loader = DataLoader(cifar_train, batch_size=batch_size, shuffle=False)
+validation_loader = DataLoader(cifar_val, batch_size=batch_size, shuffle=False)
+test_loader = DataLoader(cifar_test, batch_size= batch_size, shuffle=False)
 
 
 network = nn.Sequential(
@@ -23,18 +26,19 @@ network = nn.Sequential(
 
     nn.Conv2d(in_channels=10, out_channels=20, kernel_size=3),
     nn.LeakyReLU(),
-    nn.MaxPool2d(kernel_size=2),
+    nn.MaxPool2d(kernel_size=3),
 
     nn.Flatten(),
 
-    nn.Linear(320, 10),
+    nn.Linear(80, 10),
     nn.Softmax()
 )
 networkcopy = copy.deepcopy(network)
-optimizer = optim.SGD(network.parameters(), lr = 0.0001)
+optimizer = optim.SGD(network.parameters(), lr = 0.001)
 loss_function = nn.CrossEntropyLoss()
 traininglosses = []
 validationlosses = []
+firstRun = True
 
 for epoch in range(epochs):
     new_validationloss = 0
@@ -55,8 +59,11 @@ for epoch in range(epochs):
     images, labels = next(iter(validation_loader))
     prediction = network(images)
     new_validationloss += loss_function(prediction, labels)
-        
-    if  new_validationloss < validationloss:
+    
+    if firstRun:
+        validationloss = new_validationloss
+        firstRun = False
+    elif  new_validationloss < validationloss:
         validationloss = new_validationloss
         networkcopy = copy.deepcopy(network)
 
@@ -64,3 +71,9 @@ for epoch in range(epochs):
     traininglosses.append(loss)
     validationlosses.append(validationloss)
 
+corr = 0
+
+for index, (image, label) in enumerate(test_loader):
+    guess = torch.argmax(networkcopy(image), dim=-1)
+    corr += (guess == label).sum()
+print(corr/10000)
--- downloadDataset.py
+++ downloadDataset.py
@@ -3,17 +3,16 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 batch_size = 10000
 
-batch_size = 1000
-
-cifar_train = datasets.CIFAR10("./", train=True, download=True, transform=transforms.ToTensor())
-train_loader = DataLoader(cifar_train, batch_size=batch_size, shuffle=False)
-<<<<<<< HEAD
-cifar_validation = datasets.CIFAR10("./", train=False, download=True, transform=transforms.ToTensor())
-validation_loader = DataLoader(cifar_validation, batch_size=batch_size, shuffle=False)
-
-=======
-train_loader, validation_loader = torch.utils.data.random_split(train_loader,[4,1])
+cifar_data = datasets.CIFAR10("./", train=True, download=True, transform=transforms.ToTensor())
 
 cifar_test = datasets.CIFAR10("./", train=False, download=True, transform=transforms.ToTensor())
-test_loader = DataLoader(cifar_test, batch_size=batch_size, shuffle=False)
->>>>>>> 8d6370750f928bcbbac7c7002248a693f7ec4d66
+
+cifar_train, cifar_val = torch.utils.data.random_split(cifar_data,[40000,10000],generator=torch.Generator().manual_seed(420))
+
+
+train_loader = DataLoader(cifar_train, batch_size=batch_size, shuffle=False)
+
+validation_loader = DataLoader(cifar_val, batch_size=batch_size, shuffle=False)
+
+test_loader = DataLoader(cifar_test, batch_size= batch_size, shuffle=False)
+
