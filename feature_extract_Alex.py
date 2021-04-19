f4fc92991e5a2dd6e2905109743e2eab4e1c866e
Jonas Jacobsson <01joja@gamil.com>
2021-04-19 19:29:21 +0200

    fixed so that they saves the networks

--- feature_extract_Alex.py
+++ feature_extract_Alex.py
@@ -13,21 +13,11 @@ import pickle
 from torch.utils.tensorboard import SummaryWriter
 writer = SummaryWriter()
 
-# Make sure to remove the "ram_crash" file to get a clean run.
-# To fix ram error
-crashfile = "ram_crash"
-def save_object(network, epoch, testdata, validationdata, crashed, filename):
-    with open(filename, 'wb') as output:  # Overwrites any existing file.
-        network_dump = {"network":network, "epoch":epoch, "testdata":testdata, "validationdata":validationdata, "crashed":crashed}
-        pickle.dump(network_dump, output, pickle.HIGHEST_PROTOCOL)
-
-
-
-
 batch_size = 200
-epochs = 2
+epochs = 5
 learning_rate = 0.0001
 
+# Makes it so that the images fits the network
 preprocess = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
@@ -35,47 +25,37 @@ preprocess = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
+# Loads the data sets and devides them in to train, val and test.
 cifar_data = datasets.CIFAR10("./", train=True, download=True, transform=preprocess)
 cifar_test = datasets.CIFAR10("./", train=False, download=True, transform=preprocess)
-
 cifar_train, cifar_val = torch.utils.data.random_split(cifar_data,[40000,10000],generator=torch.Generator().manual_seed(420))
-
-
 train_loader = DataLoader(cifar_train, batch_size=batch_size, shuffle=False)
 validation_loader = DataLoader(cifar_val, batch_size=batch_size, shuffle=False)
 test_loader = DataLoader(cifar_test, batch_size= batch_size, shuffle=False)
 
-dataiter = iter(train_loader)
-images, labels = dataiter.next()
-
-#plt.imshow(np.transpose(torchvision.utils.make_grid(images).numpy(),(1,2,0)))
-#plt.show()
-
-
-
 # Import alexnet
 alex = torch.hub.load('pytorch/vision:v0.9.0', 'alexnet', pretrained=True)
-#alex.eval()
-#alex.train()
 
 # Stop learning in the original network
-#for param in alex.parameters():
-#    param.requires_grad=False
-# Creates a new hidden layer
+for param in alex.parameters():
+    param.requires_grad=False
+# Change the last layer so we get 10 classes.
 alex.classifier[6]=nn.Linear(4096,10)
 print(alex)
 
-
+# Creates the optimizer
 optimizer = optim.Adam(alex.parameters(), lr = learning_rate)
+best_model = copy.deepcopy(alex)
 loss_function = nn.CrossEntropyLoss()
+validation_loss = 9000
 
 for epoch in range(epochs):
-    new_trainingloss = 0
     i = 0
+    # Toggle training AKA turing on dropout
+    alex.train()
     for train_nr, (images, labels) in enumerate(train_loader):
         i += 1
         optimizer.zero_grad()
-
         prediction = alex(images)
         loss = loss_function(prediction, labels)
         loss.backward()
@@ -87,17 +67,33 @@ for epoch in range(epochs):
             end=''
         )
         new_trainingloss += loss.item()
-    writer.add_scalar('Adam/traininglosses', new_trainingloss/i, epoch)
-    new_validationloss = 0
-        
+    writer.add_scalar('Alex_feature/traininglosses', new_trainingloss/i, epoch)
+
+    #Toggle evaluation AKA turing off dropout
+    total_val_loss = 0
+    alex.eval()
     i = 0
     for val_nr, (images, labels) in enumerate(validation_loader):
         i += 1
         prediction = alex(images)
-        new_validationloss += loss_function(prediction, labels).item()
-    writer.add_scalar('Adam/validationloss', new_validationloss/i, epoch)
+        loss = loss_function(prediction, labels).item()
+        total_val_loss += loss
+        print(
+            '\rEpoch {} [{}/{}] - Loss: {} val'.format(
+                epoch+1, val_nr+1, len(validation_loader), loss
+            ),
+            end='                                                 '
+        )
+    
+    #Calculate the new_validationloss
+    new_validationloss = total_val_loss/i
+    if new_validationloss < validation_loss:
+        validation_loss = new_validationloss
+        best_model = copy.deepcopy(alex)
 
+    writer.add_scalar('Alex_feature/validationloss', new_validationloss/i, epoch)
 
+# Run on test data
 corr = 0
 i = 0
 for index, (image, label) in enumerate(test_loader):
@@ -105,8 +101,13 @@ for index, (image, label) in enumerate(test_loader):
     result = (guess == label).sum()
     corr += result.item()
     if 0 == (i%30):
-        print("\r", "Right guess:", 100*corr/i, "Tested pictures:", 100*i/10000,end="")
+        print("\r", "Right guess:", 100*corr/i, "Tested pictures:", 100*i/10000,end="                                                         ")
 print("\n","Result on test:", 100*corr/10000)
 writer.add_hparams({'lr': learning_rate, 'bsize': batch_size},
                     {'hparam/accuracy': 100*corr/10000})
-                    
+
+# Store the best network
+save_file = open("feature_extract_Alex_network","wb")
+pickle.dump(save_file,best_model,correctness)
+save_file.close()
+
