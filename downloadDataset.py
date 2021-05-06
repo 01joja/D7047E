9adcc9d4104e47e93e9729aab826df2926a2c6f1
kimste-6 <kimste-6@student.ltu.se>
2021-05-06 14:16:43 +0200

    que pasa hombre

---
+++ downloadDataset.py
@@ -0,0 +1,18 @@
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+batch_size = 1000
+
+cifar_data = datasets.CIFAR10("./", train=True, download=True, transform=transforms.ToTensor())
+
+cifar_test = datasets.CIFAR10("./", train=False, download=True, transform=transforms.ToTensor())
+
+cifar_train, cifar_val = torch.utils.data.random_split(cifar_data,[40000,10000],generator=torch.Generator().manual_seed(420))
+
+
+train_loader = DataLoader(cifar_train, batch_size=batch_size, shuffle=False)
+
+validation_loader = DataLoader(cifar_val, batch_size=batch_size, shuffle=False)
+
+test_loader = DataLoader(cifar_test, batch_size= batch_size, shuffle=False)
+
