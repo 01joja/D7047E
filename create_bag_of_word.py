4b0da130eb2afc082ea625faba43b105896f29cf
Jonas Jacobsson <01joja@gamil.com>
2021-05-06 15:57:04 +0200

    found an article that dose word2vec

---
+++ create_bag_of_word.py
@@ -0,0 +1,19 @@
+# https://stackabuse.com/python-for-nlp-creating-bag-of-words-model-from-scratch/
+
+
+import nltk  
+import numpy as np  
+import random  
+import string
+
+import bs4 as bs  
+import urllib.request  
+import re  
+
+
+f = open("demofile.txt", "r")
+print(f.read()) 
+
+#for para in article_paragraphs:  
+#    article_text += para.text
+
